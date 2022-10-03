e2ffe9ea6c1c922326d72947ab3f10174c7f6f98
Ally <allygoose93@gmail.com>
2022-10-03 15:18:32 -0700

    made a newclass ConferenceVO in attendees models and made some changes in api_views

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,10 +1,15 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
-from events.models import Conference
-from .models import Attendee
-from events.api_views import ConferenceListEncoder
+# from events.models import Conference
+from .models import Attendee, ConferenceVO
+# from events.api_views import ConferenceListEncoder
 from django.views.decorators.http import require_http_methods
 import json
+
+
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
 
 
 class AttendeeListEncoder(ModelEncoder):
@@ -13,9 +18,9 @@ class AttendeeListEncoder(ModelEncoder):
 
 
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
@@ -25,9 +30,10 @@ def api_list_attendees(request, conference_id):
 
         # Get the Conference object and put it in the content dict
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = f'/api/conferences/{conference_vo_id}/'
+            conference = ConferenceVO.objects.get(id=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
@@ -41,9 +47,9 @@ def api_list_attendees(request, conference_id):
         )
 
 
-class ConferenceEncoder(ModelEncoder):
-    model = Conference
-    properties = ["name"]
+# class ConferenceEncoder(ModelEncoder):
+#     model = Conference
+#     properties = ["name"]
 
 
 class AttendeeDetailEncoder(ModelEncoder):
@@ -56,7 +62,7 @@ class AttendeeDetailEncoder(ModelEncoder):
         "conference",
     ]
     encoders = {
-        "conference": ConferenceListEncoder(),
+        "conference": ConferenceVODetailEncoder(),
     }
 
 
